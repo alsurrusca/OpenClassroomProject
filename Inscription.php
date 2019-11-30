04491306b8428a9dcc0396b535c44f935ac465be
Alsurrusca <alissia.surrusca@gmail.com>
2019-11-30 10:09:27 +0100

    Avancement HTML + PHP

--- Inscription.php
+++ Inscription.php
@@ -9,18 +9,26 @@
 
     <body>
         
-    <header><img src="Images/Logo_GBAF.jpg" alt="Logo GBAF" id="Logo" /></header>
-      <p>
-          Inscription
-      <br/>
+    <header>
+        
+    <img src="Images/Logo_GBAF.jpg" alt="Logo GBAF" id="Logo" />
+    
+    </header>
+     
+    <p id="nouveau">
+          <img src="Images/icone_new_membres.png" alt title>
+          <h2>Création d'un nouveau compte<br/></h2>
+    </p>
 
-<form method="post" action="Inscription.php">
-    <fieldset>
-        <legend>Vos coordonnées</legend>
+<p>
+    <form method="post" action="Inscription.php">
+         <fieldset>
+            <legend>Vos coordonnées</legend>
     
     
      <label for="nom"> Nom : </label> 
-     <input type="text" name="nom" id="nom" required/> </p>
+     <input type="text" name="nom" id="nom" required/> 
+</p>
 
     
 
@@ -29,6 +37,9 @@
 
 <label for="username">Username :</label>
 <input type="text" name="username" id="username" required/></p>
+
+<label for="mail">Adresse Mail :</label>
+<input type="text" name="mail" id="mail" required/></p>
 
 <label for="password"> Password :</label>
 <input type="Password" name="password" id="password" required/></p>
@@ -42,6 +53,7 @@
             <option value="1">Où habitiez vous quand vous étiez petit ?</option>
             <option value="2">Le nom de jeune fille de votre mère ?</option>
             <option value="3">Le nom de votre premier animal</option>
+            
         </select> <br/>
         <label for="reponse">Réponse :</label>
         <input type="text" name="reponse" id="reponse" required/>
@@ -54,35 +66,45 @@
 </form>
 <?php
 
- if (isset($_POST['nom'])&&isset($_POST['prenom'])&&isset($_POST['username'])&& isset($_POST['password'])&&isset($_POST['question'])&& isset($_POST['reponse']))
- {
-    $nom=$_POST['nom'];
-    $prenom=$_POST['prenom'];
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-    $question=$_POST['question'];
-    $reponse=$_POST['reponse'];
-    
-    try
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=oc_gbaf;charset=utf8','root','');
-    }
-    catch (Exception $e)
-    {
-    die('Erreur :' . $e->getMessage());
-    }
+if (isset($_POST['nom'])&&isset($_POST['prenom'])&&isset($_POST['username'])&& isset($_POST['mail']) && isset($_POST['password'])&&isset($_POST['question'])&& isset($_POST['reponse']))
+{
+   $nom=$_POST['nom'];
+   $prenom=$_POST['prenom'];
+   $username=$_POST['username'];
+   $mail=$_POST['mail'];
+   $password=$_POST['password'];
+   $question=$_POST['question'];
+   $reponse=$_POST['reponse'];
+   
+   try
+   {
+       $bdd = new PDO('mysql:host=localhost;dbname=oc_gbaf;charset=utf8','root','');
+   }
+   catch (Exception $e)
+   {
+   die('Erreur :' . $e->getMessage());
+   }
+   //cache du mdp
+   $pass_hache = password_hash($_POST['password'], PASSWORD_DEFAULT);
+   
+   $req = $bdd->prepare('INSERT INTO account(nom,prenom,username,mail,password,question,reponse) VALUES (:nom, :prenom, :username, :mail, :password, :question, :reponse)');
+   $req->execute(array(
+       'nom' => $nom,
+       'prenom' => $prenom,
+       'username' => $username,
+       'mail' => $mail,
+       'password' => $pass_hache,
+       'question' => $question,
+       'reponse' => $reponse
+   ));
 
-    $req = $bdd->prepare('INSERT INTO account(nom,prenom,username,password,question,reponse) VALUES (:nom, :prenom, :username, :password, :question, :reponse)');
-    $req->execute(array(
-        'nom' => $nom,
-        'prenom' => $prenom,
-        'username' => $username,
-        'password' => $password,
-        'question' => $question,
-        'reponse' => $reponse
-    ));
 
-    echo 'Vous avez bien été enregistré, vous allez être redirigé vers la page d\' accueil, ou bien cliquez ici !';
+
+?>
+
+Vous avez bien été enregistré, vous allez être redirigé vers la page d' accueil, ou bien <a href="Index.php"> cliquez ici !</a> <br/>
+<?php
+
 
 }
 
@@ -91,6 +113,11 @@ else {
     
 }
 
+
+
         ?>
 </body>
+<footer>
+<p id="footer">Tous droits réservés | © Surrusca Alissia | V1.0 </p>
+</foooter>
 </html>
---
+++ Password_lost.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="Password_lost.css" />
+        <title>GBAF - Mot de Passe Oublié</title>
+    </head>
+
+    <header>
+                <div id="logo" class="logo">
+                     <img class="logo" src="Images/Logo.png" alt="Logo GBAF" id="logo" />
+                 </div>
+    </header>
+<body>
+                <h3> Pass perdu - Saisissez votre nom d'utilisateur.</h3>
+                <br/>
+
+                <div id="password" class="password">
+                    <form method="post" action="Password_lost.php">
+                        <p>
+                            <br/>
+                            <strong> Username : </strong>
+                            <input name="username" class="contactbox" value>
+                            <input type="hidden" name="username"> 
+                            <input type="image" src="Images/btn-ok-login.png">
+                    </form>
+</body>
+<footer>
+<p id="footer">Tous droits réservés | © Surrusca Alissia | V1.0 </p>
+</foooter>
+</html>
